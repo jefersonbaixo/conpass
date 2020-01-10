08418c0f5cf89eeb1caf3bf5fc63460220e2801a
Jeferson Baixo <jeferson.baixo@gmail.com>
2020-01-10 13:58:00 -0300

    Adicionado o prevent default e alterado de 'span' para 'a' os links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from './assets/logo.png'
 
 
 const Link = props =>
-  <span className="link" onClick={props.onClick} href={props.url}>Link fake {props.id}</span>
+  <a className="link" onClick={props.onClick} href={props.url}>Link fake {props.id}</a>
 
 const Button = props =>
   <div className="buttonArea">
@@ -17,10 +17,10 @@ class Navbar extends React.Component {
     super(props);
     this.state = {
       links: [
-        { url: 'https://conpass.io', id: 0},
-        { url: 'https://google.com', id: 1},
-        { url: 'https://facebook.com', id: 2},
-        { url: 'https://reactjs.org', id: 3},
+        { url: 'https://conpass.io', id: 0 },
+        { url: 'https://google.com', id: 1 },
+        { url: 'https://facebook.com', id: 2 },
+        { url: 'https://reactjs.org', id: 3 },
       ]
     }
   }
@@ -28,8 +28,8 @@ class Navbar extends React.Component {
   render() {
     const listItems = this.state.links.map((link, i) =>
       <Link
-        url={this.state.links[i].url} key={i} id={i}  
-        onClick={() => this.props.onClick(this.state.links[i])}
+        url={this.state.links[i].url} key={i} id={i}
+        onClick={e => this.props.onClick(e, this.state.links[i])}
       />
     );
     return (
@@ -70,27 +70,29 @@ class App extends React.Component {
     }
   }
 
-  addHotSpot(link) {
+  addHotSpot(e, link) {
+    if(!this.state.isIncludingHotspot)
+      return null;
+    e.preventDefault();
     let hotspots = this.state.hotspots.slice();
     hotspots.push(link);
-    this.setState({hotspots: hotspots})
-    console.log(this.state.hotspots)
-    this.setState({isIncludingHotspot: false});
+    this.setState({ hotspots: hotspots })
+    this.setState({ isIncludingHotspot: false });
   }
 
-  deleteHotSpot(link){
+  deleteHotSpot(link) {
     let hotspots = this.state.hotspots.slice();
-    const linkIndex = hotspots.findIndex(el => el.id === link.id);   
+    const linkIndex = hotspots.findIndex(el => el.id === link.id);
     hotspots.splice(linkIndex, 1);
-    this.setState({hotspots: hotspots});
+    this.setState({ hotspots: hotspots });
   }
 
   render() {
     return (
       <div>
-        <Navbar onClick={link => this.addHotSpot(link)} />
+        <Navbar onClick={(e, link) => this.addHotSpot(e, link)} />
         <Button onClick={() => this.setState({ isIncludingHotspot: true })} />
-        <List hotspots={this.state.hotspots} onClick={el => this.deleteHotSpot(el)}/>
+        <List hotspots={this.state.hotspots} onClick={el => this.deleteHotSpot(el)} />
       </div>
     );
   }
